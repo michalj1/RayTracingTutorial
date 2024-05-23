23fade3d1e66aa0b04b3e5d90972a83eb0ee177c
michalj1 <michalj2@illinois.edu>
2024-05-23 12:34:54 -0500

    Initial commit

---
+++ Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour {
+    public ComputeShader RayTracingShader;
+
+    private RenderTexture _target;
+    private Camera _camera;
+
+
+    /* Rendering functions */
+
+    /*
+    Calls custom pipeline after Unity pipeline is finished rendering
+    */
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    /*
+    Uses shader to render to the screen
+    */
+    private void Render(RenderTexture destination) {
+        // Creates target and informs shader
+        InitRenderTexture();
+        RayTracingShader.SetTexture(0, "Result", _target);
+
+        // Execute shader with one thread per 8x8 pixel area on the screen
+        int numThreadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int numThreadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, numThreadGroupsX, numThreadGroupsY, 1);
+
+        // Display on screen
+        Graphics.Blit(_target, destination);
+    }
+
+    /*
+    Creates a render target with the correct dimensions
+    */
+    private void InitRenderTexture() {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height) {
+            if (_target != null) _target.Release();
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+
+    /* Camera functions */
+
+    /*
+    Gets camera when scene is loaded
+    */
+    private void Awake() {
+        _camera = GetComponent<Camera>();
+    }
+
+    /*
+    Sets matrices in the shader
+    */
+    private void SetShaderParameters() {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+    }
+}
